beee023503f51590505b64870fe956310118ea48
MarcosVel <marcos.veloso@sempreceub.com>
2021-09-11 14:48:06 -0300

    add loading

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from 'react';
-import { FlatList, StyleSheet, Text, View } from 'react-native';
+import { ActivityIndicator, FlatList, StyleSheet, Text, View } from 'react-native';
 
 import api from './src/services/api';
 import Filmes from './src/filmes';
 
 export default function App() {
-  const [ filmes, setFilmes ] = useState([])
+  const [ filmes, setFilmes ] = useState([]);
+  const [ loading, setLoading ] = useState(true);
 
   useEffect(() => {
 
@@ -13,28 +14,42 @@ export default function App() {
       const response = await api.get('r-api/?api=filmes')
       // console.log(response.data);
       setFilmes(response.data);
+      setLoading(false);
     }
 
     loadFilmes();
   }, []);
 
-  return (
-    <View style={ styles.container }>
-      <FlatList
-        data={ filmes }
-        keyExtractor={ item => String(item.id) } // passando o id para String - preferível pelo FlatList
-        renderItem={ ({ item }) =>
-          <Filmes
-            data={ item }
-          />
-        }
-        contentContainerStyle={ { paddingBottom: 30 } }
-      />
-    </View>
-  )
+  if (loading) {
+    return (
+      <View style={ styles.loading }>
+        <ActivityIndicator color='#121212' size={ 45 } />
+      </View>
+    )
+  } else {
+    return (
+      <View style={ styles.container }>
+        <FlatList
+          data={ filmes }
+          keyExtractor={ item => String(item.id) } // passando o id para String - preferível pelo FlatList
+          renderItem={ ({ item }) =>
+            <Filmes
+              data={ item }
+            />
+          }
+          contentContainerStyle={ { paddingBottom: 30 } }
+        />
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
+  loading: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
   container: {
     flex: 1,
   }
